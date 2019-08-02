1a95e5e847a0b807ac39e712ad981761524e74f3
Arli <arli.machado@gmail.com>
2019-08-02 15:47:04 -0300

    Definindo serviço para listagem de providers

--- gobarber/src/app/models/File.js
+++ gobarber/src/app/models/File.js
@@ -1,11 +1,22 @@
 import Sequelize, { Model } from 'sequelize';
 
+require('dotenv').config({
+  path: process.env.NODE_ENV === 'test' ? '.env.test' : '.env',
+});
+
 class File extends Model {
   static init(sequelize) {
+    const { DOMAIN, PORT } = process.env;
     super.init(
       {
         name: Sequelize.STRING,
         path: Sequelize.STRING,
+        url: {
+          type: Sequelize.VIRTUAL,
+          get() {
+            return `http://${DOMAIN}:${PORT}/files/${this.path}`;
+          },
+        },
       },
       {
         sequelize,
